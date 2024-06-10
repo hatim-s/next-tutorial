89663359036b6261fc660558a2bc9e2d66a79257
Hatim Shakir <hatimshakir9179@gmail.com>
2024-06-10 18:37:09 +0530

    Init commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Customers = (props: Props) => {
+  return <p>Customers Page</p>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Invoices = (props: Props) => {
+  return <p>Invoices Page</p>;
+};
+
+export default Invoices;
